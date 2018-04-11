7ef2422569946b0a21c358c9c878b2bc558f0a7b
Josh Juran <jjuran@metamage.com>
2018-04-11 08:09:06 -0400

    Replace RoutineDescriptor with real SndCallBackUPP

--- Source/Sound.c
+++ Source/Sound.c
@@ -127,9 +127,6 @@ void PlayExternalSound (short soundID, short priority)
 // of the interupt situation, we need to handle setting A5 to point to our
 // app's A5 and then set it back again.
 
-RoutineDescriptor ExternalCallBackRD =
-		BUILD_ROUTINE_DESCRIPTOR(uppSndCallBackProcInfo, ExternalCallBack);
-
 pascal void ExternalCallBack (SndChannelPtr theChannel, SndCommand *theCommand)
 {
 	long savedA5 = SetA5(theCommand->param2);
@@ -213,7 +210,15 @@ OSErr OpenSoundChannel (void)
 	OSErr		theErr;
 	
 	#if TARGET_RT_MAC_CFM
-		externalCallBackUPP = (SndCallBackUPP) &ExternalCallBackRD;
+		if (! externalCallBackUPP)
+		{
+			externalCallBackUPP = NewSndCallBackUPP(ExternalCallBack);
+			
+			if (! externalCallBackUPP)
+			{
+				return memFullErr;
+			}
+		}
 	#else
 		externalCallBackUPP = &ExternalCallBack;
 	#endif
